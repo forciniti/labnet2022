6d8cce556035b635900688df276348f104b35b19
forciniti <forcinitiit38@gmail.com>
2022-09-16 01:45:14 -0300

    Código de ingresar pasajeros a transporte publico
    Se sube código, archivos de solución y proyecto de Ingresar pasajeros a transporte publico ejercicio

--- EjercicioPOOLAB-NET-2022/EjercicioPOOLAB-NET-2022/Program.cs
+++ EjercicioPOOLAB-NET-2022/EjercicioPOOLAB-NET-2022/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EjercicioPOOLAB_NET_2022
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
